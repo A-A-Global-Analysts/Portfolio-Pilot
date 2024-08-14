32e03e2a29b394d1408102188a9f561ff9e0a587
LeonKong14 <lingenkong@hotmail.com>
2024-08-14 09:09:54 -0400

    Remove stock usecase implemented

---
+++ src/use_case/remove_stock/RemoveStockOutputData.py
@@ -0,0 +1,6 @@
+class RemoveStockOutputData:
+    def __init__(self, tickers_to_quantities):
+        self.tickers_to_quantities = tickers_to_quantities
+
+    def get_tickers_to_quantities(self):
+        return self.tickers_to_quantities
---
+++ src/use_case/remove_stock/RemoveStockUserDataAccessInterface.py
@@ -0,0 +1,5 @@
+from entity import User
+
+class RemoveStockUserDataAccessInterface:
+    def get_user_from_username(self, username: str) -> User:
+        pass
